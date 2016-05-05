a523cb6c2f81afe98b001a00b5121c33b5a05d8e
farisodobasic <akiselica1@etf.unsa.ba>
2016-05-05 02:36:16 +0200

    meh

--- pis/cabhome.php
+++ pis/cabhome.php
@@ -1,1 +1,40 @@
+<?php
+	session_start();
+	include_once 'dbconnect.php';
 
+	if(!isset($_SESSION['user']))
+	{	
+	 header("Location: index.php");
+	}
+
+	$res=mysql_query("SELECT * FROM users WHERE id=".$_SESSION['user']);
+	$userRow=mysql_fetch_array($res);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>CHANGE MANAGEMENT</title>
+	<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+	<div id = "welcome-title">Welcome <?php echo $userRow['username']; ?> &nbsp;	</div>
+	<div id = "top">
+		<h2>Change Management</h2>
+	</div>
+
+	<div id = "user-frame-div">
+		<div id = "user-menu-div">
+			 <ul>
+			  <li><a href="#">OPTION 4</a></li>
+			  <li><a href="#">OPTION 5</a></li>
+			  <li><a href="logout.php?logout">LOGOUT</a></li>
+			</ul> 
+		</div>
+
+		<div id = "user-dashboard-div">
+			meh2
+		</div>
+	</div>
+</body>
+</html>
