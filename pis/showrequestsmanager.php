f02ef17bbb3611dc02233ee1b46a5428b49aa009
farisodobasic <akiselica1@etf.unsa.ba>
2016-05-05 18:08:11 +0200

    a

--- pis/showrequestsmanager.php
+++ pis/showrequestsmanager.php
@@ -6,9 +6,15 @@
 	{	
 	 header("Location: index.php");
 	}
-
 	$res=mysql_query("SELECT * FROM users WHERE id=".$_SESSION['user']);
 	$userRow=mysql_fetch_array($res);
+
+	if(isset($_POST['btn-accept']))
+	{
+		$changeis = $_POST['userchangeid'];
+		/*echo $ime;*/
+		
+	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -34,29 +40,109 @@
 		<div id = "user-dashboard-div">
 			
 			<?php
-				$veza = new PDO("mysql:dbname=bdchange;host=localhost;charset=utf8", "root", "root");
+				$veza = new PDO("mysql:dbname=dbchange;host=localhost;charset=utf8", "admin", "admin");
 				$veza->exec("set names utf8");
-				$rezultat = $veza->query("select id, username, email from users");
+				$rezultat = $veza->query("select id, username, email, type from users");
 
 				foreach ($rezultat as $user) 
 				{
-					$ime = $user['username'];
-
-					print("<div id = 'show-user-request'>".$ime."</div>");
+					if($user['type'] == 0)
+					{
+						$ime = $user['username'];
+						$id = $user['id'];
+						/*SELEKTOVANJE CHANGE-ova SVIH USERA*/
+						/*print("<div id = 'show-user-request'>".$ime."</div>");*/
+						$result = $veza->query("SELECT id, naslov, UNIX_TIMESTAMP(datum) vrijeme FROM `change` where user_id = '$id'");
+						print "<div id = 'show-user-request'>
+									<div id = 'naslov-ime-usera'>
+										".$ime."
+									</div>";
+									foreach ($result as $changer) 
+									{
+										$naslov = $changer['naslov'];
+										$datum = date("h:m:Y (h:i)", $changer['vrijeme']);
+										$changeid = $changer['id'];
+										
+										print "<div id = 'show-user-changes'>";
+													print $naslov;
+													print "<br><br>";
+													print $datum;
+													print "<form method = 'post'>
+														<table align='center'>
+															<tr>
+																<td><button type='submit' name='btn-accept'>Accept</button></td>
+																<td><input type='text' value='".$changeid."' name='userchangeid' style='display:none;'></button></td>
+															</tr>
+														</table>
+													</form>
+												</div>";
+									}
+										
+						print"</div>";
+					}
 				}
 			?>
 			<!--<div id = "show-user-request">
-				testic
-			</div>
-			<div id = "show-user-request">
-				testic
-			</div>
-			<div id = "show-user-request">
-				testic
-			</div>
-			<div id = "show-user-request">
-				testic
+				<div id = "naslov-ime-usera">
+					Faris
+				</div>
+				<div id = "show-user-changes">
+					Naslov<br><br>
+					Datum
+					<form method = "post">
+						<table align="center">
+							<tr>
+								<td><button type="submit" name="btn-accept">Accept</button></td>
+								<td><input type="text" value="faris" name="txt" style="display:none;"></button></td>
+							</tr>
+						</table>
+					</form>
+				</div>
 			</div>-->
+
+			<!--<div id = "show-user-request">
+				<div id = "naslov-ime-usera">
+					Orhan
+				</div>
+
+				<div id = "show-user-changes">
+					<div id = "user-change-manager">
+						Naslov<br><br>
+						Datum
+						<form method = "post">
+							<table align="center">
+								<tr>
+									<td><button type="submit" name="btn-accept">Accept</button></td>
+									<td><input type="text" value="orhan" name="userchangeid" style="display:none;"></button></td>
+								</tr>
+							</table>
+						</form>
+					</div>
+				</div>
+			</div>
+
+			<div id = "show-user-request">
+				<div id = "naslov-ime-usera">
+					Orhan
+				</div>
+
+				<div id = "show-user-changes">
+					<div id = "user-change-manager">
+						Naslov<br><br>
+						Datum
+						<form method = "post">
+							<table align="center">
+								<tr>
+									<td><button type="submit" name="btn-accept">Accept</button></td>
+									<td><input type="text" value="faris" name="userchangeid" style="display:none;"></button></td>
+								</tr>
+							</table>
+						</form>
+					</div>
+				</div>
+			</div>-->
+
+
 		</div>
 	</div>
 </body>
